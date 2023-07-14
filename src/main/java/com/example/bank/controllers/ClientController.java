ebb18f8f8c3310a2b6cec28dc32bfcee3c8a2134
homepirate <el2409@yandex.ru>
2023-07-14 17:49:17 +0300

    add client controller

---
+++ src/main/java/com/example/bank/controllers/ClientController.java
@@ -0,0 +1,31 @@
+package com.example.bank.controllers;
+
+import com.example.bank.dtos.ClientDto;
+import com.example.bank.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ClientController {
+    @Autowired
+    private ClientService clientService;
+
+    @PostMapping("/add-client")
+    ClientDto newClient(@RequestBody ClientDto newClient){
+        return clientService.addClient(newClient);
+    }
+    @GetMapping("/clients/{id}")
+    ClientDto one(@PathVariable Integer id){
+        return clientService.findClientById(id);
+    }
+
+    @DeleteMapping("/client-delete/{id}")
+    void delete(@PathVariable Integer id){
+        clientService.removeClientById(id);
+    }
+
+    @PutMapping("/client--phonenumber-update/{id}")
+    ClientDto updatePhoneNumber( @RequestBody String phoneNumber, @PathVariable Integer id){
+        return clientService.updatePhoneNumberById(id, phoneNumber);
+    }
+}
