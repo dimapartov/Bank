d2d6b43e494999b58c5d48a8b46a415f25240596
dimapartov <mr_kubarev@mail.ru>
2023-07-14 19:14:09 +0300

    fix repo annotation

--- src/main/java/com/example/bank/repositories/ClientTransactionRepository.java
+++ src/main/java/com/example/bank/repositories/ClientTransactionRepository.java
@@ -1,6 +1,8 @@
 package com.example.bank.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientTransactionRepository extends JpaRepository<ClientRepository, Integer> {
 }
